66ecd2f729a6396a1bcc7da0eec29ebf82874f62
Se min Lee <mlsml94@gmail.com>
2020-09-28 13:23:53 -0400

    auth & user controller

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,20 +1,20 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :username, :name, :address
 
-  def initialize(user_object)
-    @user = user_object
-  end
+  # def initialize(user_object)
+  #   @user = user_object
+  # end
 
-  def to_serialized_json
-    @user.to_json(
-      :include => {
-        :follower_follows => { 
-          :include => [:follower]
-        },
-        :followee_follows => { 
-          :include => [:followee]
-        }
-    }, :except => [:password])
-  end
+  # def to_serialized_json
+  #   @user.to_json(
+  #     :include => {
+  #       :follower_follows => { 
+  #         :include => [:follower]
+  #       },
+  #       :followee_follows => { 
+  #         :include => [:followee]
+  #       }
+  #   }, :except => [:password])
+  # end
 
 end
