8d18d7305e3baf98665a116a07008e4286dc2697
Se min Lee <mlsml94@gmail.com>
2020-09-28 20:55:09 -0400

    auth

--- app/controllers/Api/V1/auth_controller.rb
+++ app/controllers/Api/V1/auth_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
    
-    def create
-      @user = User.find_by(email: user_login_params[:email])
-      if @user && @user.authenticate(user_login_params[:password])
+    def login
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
         token = encode_token({ user_id: @user.id })
         render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
       else
-        render json: { message: 'Invalid email or password' }, status: :unauthorized
+        render json: { message: 'Invalid username or password' }, status: :unauthorized
       end
     end
    
     private
    
     def user_login_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:username, :password)
     end
   end
